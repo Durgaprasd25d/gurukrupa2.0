2d40ec912f59639b35485bc051f021c919791051
Durgaprasad Dalai <durgaprasaddalai10@gmail.com>
2024-04-09 18:54:12 +0530

    initial creation for exams section

---
+++ questions.js
@@ -0,0 +1,65 @@
+const questions = [
+  
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "Rome", "Berlin", "London"],
+      correctAnswer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Venus", "Mars", "Jupiter", "Saturn"],
+      correctAnswer: "Mars",
+    },
+    {
+      question: "Who painted the Mona Lisa?",
+      options: [
+        "Vincent van Gogh",
+        "Leonardo da Vinci",
+        "Pablo Picasso",
+        "Claude Monet",
+      ],
+      correctAnswer: "Leonardo da Vinci",
+    },
+    {
+      question: "What is the largest ocean on Earth?",
+      options: [
+        "Atlantic Ocean",
+        "Indian Ocean",
+        "Arctic Ocean",
+        "Pacific Ocean",
+      ],
+      correctAnswer: "Pacific Ocean",
+    },
+    {
+      question: "Which country is known as the 'Land of the Rising Sun'?",
+      options: ["China", "India", "Japan", "South Korea"],
+      correctAnswer: "Japan",
+    },
+    {
+      question: "Who is credited with discovering gravity?",
+      options: [
+        "Isaac Newton",
+        "Albert Einstein",
+        "Galileo Galilei",
+        "Stephen Hawking",
+      ],
+      correctAnswer: "Isaac Newton",
+    },
+    {
+      question: "What year did the Titanic sink?",
+      options: ["1912", "1920", "1908", "1935"],
+      correctAnswer: "1912",
+    },
+    {
+      question: "Which mammal can fly without feathers?",
+      options: ["Bat", "Sparrow", "Penguin", "Ostrich"],
+      correctAnswer: "Bat",
+    },
+    {
+      question: "What is the chemical symbol for gold?",
+      options: ["Au", "Ag", "Fe", "Cu"],
+      correctAnswer: "Au",
+    },
+   
+  ];
+  
